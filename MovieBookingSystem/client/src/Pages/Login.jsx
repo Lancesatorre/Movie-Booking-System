2c802eb678a1b95a069c70c3aeaa6424cdfa06b9
EuwenAldrichVillarin <euwenaldrich123@gmail.com>
2025-11-29 22:51:36 +0800

    feat(Villarin)

--- MovieBookingSystem/client/src/Pages/Login.jsx
+++ MovieBookingSystem/client/src/Pages/Login.jsx
@@ -358,7 +358,7 @@ export default function Login() {
                     Logging in...
                   </div>
                 ) : (
-                  'Login'
+                  'WOW'
                 )}
               </button>
             </div>
